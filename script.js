2883fe18128038201eb3cb3037a2485facd6e6d1
MichalZin <124308169+MichalZin@users.noreply.github.com>
2024-02-15 19:06:56 +0100

    Improvement of palindrome function

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 
 function palCheck(string) {
+    function reverseString(string) {
+        let copy = string.split("")
+        .reverse()
+        .join("")
+        return copy;
+    }
+    /*
     function reverse(string) {
         let filter = /^[a-zA-Z0-9]+$/;
         let exchange = string.match(filter)
@@ -9,13 +16,13 @@ function palCheck(string) {
         join("");
         return exchange
     }
-    
+    */
 
-    if (stringOne === reverse(string)) {
-        console.log("It's a palindrome")
+    if (string === reverseString(string)) {
+        console.log("It's a palindrome");
     }
     else {
-        console.log("It's not a palindrome")
+        console.log("It's not a palindrome");
     }
 
 }

01827959b28745b96353abae98d590bd2e3e18fd
MichalZin <124308169+MichalZin@users.noreply.github.com>
2024-02-17 10:02:05 +0100

    Setting up and testing function

--- script.js
+++ script.js
@@ -1,28 +1,30 @@
+const textCheck = document.getElementById('text-input')
+const subButton = document.getElementById('check-btn');
+const testResult = document.getElementById('result');
 
 function palCheck(string) {
     function reverseString(string) {
-        let copy = string.split("")
+        let stringFilter = string.match(/[a-zA-Z0-9]+/g)
+            .join("")
+            .toLowerCase();
+        let stringRev = stringFilter.split("")
         .reverse()
-        .join("")
-        return copy;
+        .join("");
+        return stringRev;
     }
-    /*
-    function reverse(string) {
-        let filter = /^[a-zA-Z0-9]+$/;
-        let exchange = string.match(filter)
-        .toUpperCase()
-        .split("")
-        .reverse().
-        join("");
-        return exchange
-    }
-    */
 
     if (string === reverseString(string)) {
-        console.log("It's a palindrome");
+        let opResultPositive = `${string} is a palindrome`
+        return opResultPositive;
     }
     else {
-        console.log("It's not a palindrome");
+        let opResultNegative = `${string} is not a palindrome`
+        return opResultNegative;
     }
 
 }
+
+subButton.addEventListener("click", () => {
+    let finalResult = palCheck(textCheck.value);
+    testResult.innerHTML = finalResult;
+})

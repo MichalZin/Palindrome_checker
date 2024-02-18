9700e45166f8396af97e44008e8e4f6871790e94
MichalZin <124308169+MichalZin@users.noreply.github.com>
2024-02-18 11:52:09 +0100

    Swapping function

--- script.js
+++ script.js
@@ -1,55 +1,28 @@
 const textCheck = document.getElementById('text-input')
 const subButton = document.getElementById('check-btn');
 const testResult = document.getElementById('result');
-/*
+
 function palCheck(string) {
-     function reverseString(string) {
-        let str = string
-        let stringFilter = str.replace(/[^a-zA-Z0-9]/g, '')
-            .toLowerCase();
-        let stringRev = stringFilter.split("")
+let cleanString = string.replace(/\s/g, '');
+let strNonAl = cleanString.replace(/[^a-zA-Z0-9]/g, '');
+let strFin = strNonAl.toUpperCase();
+
+let revStr = strFin.split("")
         .reverse()
         .join("");
-        return stringRev;
-    }
-    */
-   
-    function reverseString(string) {
-        let stringFilter = stringNoSpaces.replace(/[^a-zA-Z0-9]/\s/g, '').toLowerCase();
-        let stringRev = stringFilter.split("").reverse().join("");
-        return stringRev;
-    }
-    
-    function palCheck(string) {
 
-        let stringWithoutSpaces = string.replace(/\s/g, '')
-        if (!string) {
-            return "Please input a value";
-        } else if (stringWithoutSpaces === reversedString) {
-            return `${string} is a palindrome`;
-        } else {
-            return `${string} is not a palindrome`;
-        }
-    }
-
-    /*
-    if (!string) {
-        let noResult = `Please input a value`
-        return noResult;
-    }
-    else if (string === reverseString(string)) {
-        let opResultPositive = `${string} is a palindrome`
-        return opResultPositive;
-    }
-    else {
-        let opResultNegative = `${string} is not a palindrome`
-        return opResultNegative;
-    }
+if(!string) {
+   alert("Please input a value") 
+}
+else if (strFin === revStr){
+    testResult.innerText = `${string} is a palindrome`
+}
+else {
+    testResult.innerText = `${string} is not a palindrome`
+}
 
 }
-*/
-subButton.addEventListener("click", () => {
-    let inputString = textCheck.value
-    let finalResult = palCheck(inputString);
-    testResult.innerText = finalResult;
-})
+
+subButton.onclick = function () {
+    palCheck(textCheck.value)
+};

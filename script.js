0dadd6ee9dc800805a1d2ca34c2d91cbd469f13d
MichalZin <124308169+MichalZin@users.noreply.github.com>
2024-02-18 09:21:20 +0100

    Allowing multi world strings to be used

--- script.js
+++ script.js
@@ -1,19 +1,43 @@
 const textCheck = document.getElementById('text-input')
 const subButton = document.getElementById('check-btn');
 const testResult = document.getElementById('result');
-
+/*
 function palCheck(string) {
-    function reverseString(string) {
-        let stringFilter = string.match(/[a-zA-Z0-9]+/g)
-            .join("")
+     function reverseString(string) {
+        let str = string
+        let stringFilter = str.replace(/[^a-zA-Z0-9]/g, '')
             .toLowerCase();
         let stringRev = stringFilter.split("")
         .reverse()
         .join("");
         return stringRev;
     }
+    */
+   
+    function reverseString(string) {
+        let stringFilter = stringNoSpaces.replace(/[^a-zA-Z0-9]/\s/g, '').toLowerCase();
+        let stringRev = stringFilter.split("").reverse().join("");
+        return stringRev;
+    }
+    
+    function palCheck(string) {
 
-    if (string === reverseString(string)) {
+        let stringWithoutSpaces = string.replace(/\s/g, '')
+        if (!string) {
+            return "Please input a value";
+        } else if (stringWithoutSpaces === reversedString) {
+            return `${string} is a palindrome`;
+        } else {
+            return `${string} is not a palindrome`;
+        }
+    }
+
+    /*
+    if (!string) {
+        let noResult = `Please input a value`
+        return noResult;
+    }
+    else if (string === reverseString(string)) {
         let opResultPositive = `${string} is a palindrome`
         return opResultPositive;
     }
@@ -23,8 +47,9 @@ function palCheck(string) {
     }
 
 }
-
+*/
 subButton.addEventListener("click", () => {
-    let finalResult = palCheck(textCheck.value);
-    testResult.innerHTML = finalResult;
+    let inputString = textCheck.value
+    let finalResult = palCheck(inputString);
+    testResult.innerText = finalResult;
 })
